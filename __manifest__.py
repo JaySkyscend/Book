c18ce946843aeb4eaf3882f60cccb357f7ad032b
JaySkyscend <jaypatel.skyscendbs@gmail.com>
2025-03-13 14:21:54 +0530

    Added to git

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name':'Book Shop Management',
+    'version':'1.0',
+    'summary':'Manage book inventory, authors , and sales',
+    'author': 'Jay Patel',
+    'category':'Custom',
+    'depends':['sale'],
+    'data' : [
+
+
+    ]
+
+}
---
+++ models/book.py
@@ -0,0 +1,7 @@
+from odoo import models, fields
+
+class Book(models.Model):
+    _name = 'book.shop.book'
+    _description = 'Book Details'
+
+    n
